1d23bffcd607bf83e1f8ea262281f09675ad8511
Zigfruid <Zigfruid@gmail.com>
2020-08-21 23:13:40 +0500

    profile fragment was done

--- app/src/main/java/com/example/socailnetwork/fragments/AccountFragment.kt
+++ app/src/main/java/com/example/socailnetwork/fragments/AccountFragment.kt
@@ -1,8 +1,45 @@
 package com.example.socailnetwork.fragments
 
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.socailnetwork.R
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.account_fragment.*
+
 
 class AccountFragment : Fragment(R.layout.account_fragment) {
+   private val db = FirebaseFirestore.getInstance()
+    private val mAuth = FirebaseAuth.getInstance()
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        showData()
+        btnSave.setOnClickListener {
+            val map: MutableMap<String, Any> = mutableMapOf()
+            map["username"] = etUserName.text.toString()
+            map["email"] = etEmailAddress.text.toString()
+            map["phone"] = etPhoneNumber.text.toString()
+            map["info"] = etInformation.text.toString()
+            db.collection("users").document(mAuth.currentUser!!.uid).set(map)
+                .addOnSuccessListener {
+                    Toast.makeText(requireContext(), "Профильге озгерис киритилди", Toast.LENGTH_SHORT).show()
+                }
+                .addOnFailureListener {
+                    Toast.makeText(requireContext(), it.localizedMessage, Toast.LENGTH_SHORT).show()
 
+                }
+        }
+    }
+
+   private fun showData(){
+        db.collection("users").document(mAuth.currentUser!!.uid).get()
+            .addOnSuccessListener {
+                 etUserName.setText(it.get("username").toString())
+                 etEmailAddress.setText(it.get("email").toString())
+                 etPhoneNumber.setText(it.get("phone").toString())
+                 etInformation.setText(it.get("info").toString())
+            }
+    }
 }
--- app/src/main/java/com/example/socailnetwork/ui/MainActivity.kt
+++ app/src/main/java/com/example/socailnetwork/ui/MainActivity.kt
@@ -1,18 +1,42 @@
 package com.example.socailnetwork.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.socailnetwork.R
 import com.example.socailnetwork.fragments.AccountFragment
 import com.example.socailnetwork.fragments.AddPostFragment
 import com.example.socailnetwork.fragments.HomeFragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private val mAuth = FirebaseAuth.getInstance()
+    private  val db = FirebaseFirestore.getInstance()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        db.collection("users").document(mAuth.currentUser?.uid!!.toString()).get()
+            .addOnCompleteListener {
+                if (it.isSuccessful && !it.result?.exists()!!){
+                    val map: MutableMap<String, Any?> = mutableMapOf()
+                    map["email"] = mAuth.currentUser?.email
+
+                        db.collection("users").document(mAuth.currentUser?.uid!!).set(map)
+                            .addOnSuccessListener {
+                                Log.d("user", "User has been added successfully")
+                            }
+                            .addOnFailureListener {e->
+                                Log.d("user", e.localizedMessage!!.toString())
+
+                            }
+                }
+            }
 
         val homeFragment = HomeFragment()
         val addPostFragment = AddPostFragment()
